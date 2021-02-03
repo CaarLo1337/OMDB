1f7957bc8b69af84f644e7f6add2638a6b9bfccc
CaarLo1337 <carlobeck96@outlook.de>
2021-02-03 23:13:35 +0100

    -

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+const request = require("request");
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+//___________________________________________________
+
 app.use(express.static("lib"));
 
 app.set("view engine", "ejs");
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const request = require("request");
 
-app.listen(3000, function() {
+app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
 
-app.get("/", function(request, response) {
-    response.render("page");
+app.get("/", (req, res) => {
+    res.render("page");
 });
 
-app.get("*", function(request, response) {
-    response.render("error");
+app.get("/results", (req, res) => {
+    const searchedMovie = req.query.searchValue;
+    const queryString = "http://omdbapi.com/?s=" + searchedMovie + "&apikey=" + process.env.API_KEY;
+    
+    
+    request(queryString, function(error, response, body){
+    if(!error && response.statusCode == 200){
+        var parseData = JSON.parse(body);
+        //console.log(parseData["Search"][0]["Title"]);
+        res.render("results", {data: parseData});
+    }
+    });
+});
+
+app.get("*", (req, res) => {
+    res.render("error");
 });
 
