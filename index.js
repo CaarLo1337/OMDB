f0136e855ce8ff21d0ba01496a137cf649200415
CaarLo1337 <carlobeck96@outlook.de>
2021-02-24 20:34:10 +0100

    printing random movieID

--- index.js
+++ index.js
@@ -21,22 +21,33 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req, res) => {
-    //const movieList = 'http://omdbapi.com/?i=tt' + Math.floor(Math.random() * 1285017 + 1285000) + '&apikey=' + process.env.API_KEY;
+    var randomId = Math.floor(Math.random() * (1285017 - 1285000) + 1285000); //random number between 1285017 and 1285000
+    //console.log(randomId.toString()); // print movieID
+    randomMovie = 'http://omdbapi.com/?i=tt' + randomId.toString() + '&apikey=' + process.env.API_KEY;
+    var result;
+    rp(randomMovie)
+    .then((body) => {
+        result = JSON.parse(body)
+        //console.log(JSON.stringify(result)); //print json in console
+    })
+    
     res.render("page");
 });
 
 app.get("/results", (req, res) => {
-    const searchedMovie = 'http://omdbapi.com/?s=' + req.query.movie + '&apikey=' + process.env.API_KEY;
+    const searchedMovie = 'http://omdbapi.com/?s=' + req.query.movie + '&apikey=' + process.env.API_KEY; 
     const movieDetails = [];
     var results;
     rp(searchedMovie)
     .then((body) => {
         results = JSON.parse(body);
+        //console.log(JSON.stringify(results)); //print json in console
         if(results['Response']=='True'){
             for(let i=0; i<results['Search'].length; i++) {
                 rp('http://omdbapi.com/?i=' + results['Search'][i]['imdbID'] + '&apikey=' + process.env.API_KEY)
                 .then(data => {
                     movieDetails.push(JSON.parse(data));
+                    //console.log(JSON.stringify(movieDetails)); //print json in console
                     if(movieDetails.length === results['Search'].length){
                         res.render('resultspage',{results: results, keyword: req.query.movie, movieDetails: movieDetails});
                     }
