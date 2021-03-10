c7e3fc2a7b655d8db589f577e25a5345d67d59bd
CaarLo1337 <carlobeck96@outlook.de>
2021-03-10 21:03:45 +0100

    removed debug logs

--- src/api/helpers/isLoggedIn.js
+++ src/api/helpers/isLoggedIn.js
@@ -3,25 +3,25 @@ const jwt = require('jsonwebtoken');
 module.exports = function(req, res, next) {
     const token = req.cookies.accessToken || '';
     if(token) {
-        console.log('-------new try-----\n')
-        console.log('token true');
-        console.log(token);
+        //console.log('-------new try-----\n')
+        //console.log('token true');
+        //console.log(token);
         try {
-            console.log(req.user);
-            const verified = jwt.verify(token, process.env.JWT_TOKEN);
-            req.user = verified;
-            console.log('\nverified req.user');
-            console.log(req.user);
-            res.locals.loggedIn = req.user;
+            //console.log(req.user);
+            const verified = jwt.verify(token, process.env.JWT_TOKEN); // check if token is valid
+            req.user = verified; 
+            //console.log('\nverified req.user');
+            //console.log(req.user);
+            res.locals.loggedIn = req.user; 
             next();
         } catch(err) {
-            console.log('\ninvalid token');
+            //console.log('\ninvalid token');
             res.locals.loggedIn = null;
             next();
         }
     } else {
-        console.log('\nno token');
-        console.log(token)
+        //console.log('\nno token');
+        //console.log(token)
         res.locals.loggedIn = null;
         next();
     }
