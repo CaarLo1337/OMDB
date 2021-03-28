648d4d4b3a34ce717431df414e470cf8ba6fc36c
CaarLo1337 <carlobeck96@outlook.de>
2021-03-28 20:25:19 +0200

    -

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,8 +1,15 @@
+const User = require("../models/user.model");
+
 module.exports = {
     profile_get
 }
 
-function profile_get(req, res) {
-    console.log(user.role);
+async function profile_get(req, res) {
+    // some testing
+    req.user = await User.findOne();
+    const userdata = req.user
+    console.log(userdata.role);
+
+    // render profilepage
     res.render('profile.ejs');
 }
